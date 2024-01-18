8d515481cc9220559f9ec295ec40cba3941fb20e
Matias Monteverde <initmonte@gmail.com>
2024-01-17 23:57:55 -0300

    fixed router to pages

--- api/routes/pages.js
+++ api/routes/pages.js
@@ -3,11 +3,11 @@ import { Router } from 'express'
 export const pagesRouter = Router()
 
 pagesRouter.get('/', (req, res) => {
-  res.send('<h1>Mi HOME</h1>')
+  res.sendFile('api/pages/home.html', { root: '.' })
 })
 pagesRouter.get('/docs', (req, res) => {
-  res.send('<h1>Mi DOCS</h1>')
+  res.sendFile('api/pages/docs.html', { root: '.' })
 })
 pagesRouter.get('/*', (req, res) => {
-  res.send('<h1>ERROOOOOOOR not found</h1>')
+  res.sendFile('api/pages/notFound.html', { root: '.' })
 })
