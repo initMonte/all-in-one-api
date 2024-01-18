282c1f42d2a02319f3ca805185c0fb9f2374ec3e
Matias Monteverde <initmonte@gmail.com>
2024-01-18 00:15:33 -0300

    Change route

--- api/routes/pages.js
+++ api/routes/pages.js
@@ -4,17 +4,17 @@ import fs from 'node:fs'
 export const pagesRouter = Router()
 
 pagesRouter.get('/', (req, res) => {
-  const home = fs.readFileSync('api/pages/home.html')
+  const home = fs.readFileSync('/api/pages/home.html')
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(home)
 })
 pagesRouter.get('/docs', (req, res) => {
-  const docs = fs.readFileSync('api/pages/docs.html')
+  const docs = fs.readFileSync('/api/pages/docs.html')
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(docs)
 })
 pagesRouter.get('/*', (req, res) => {
-  const notFound = fs.readFileSync('api/pages/notFound.html')
+  const notFound = fs.readFileSync('/api/pages/notFound.html')
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(notFound)
 })
