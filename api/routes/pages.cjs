284c7c051f428f76320b05dccded8d5b27a4a81a
Matias Monteverde <initmonte@gmail.com>
2024-01-18 01:19:39 -0300

    does this work?

--- api/routes/pages.cjs
+++ api/routes/pages.cjs
@@ -1,10 +1,12 @@
 const express = require('express')
 const fs = require('node:fs')
+const path = require('node:path')
 
 const pagesRouter = express.Router()
 
 pagesRouter.get('/', (req, res) => {
-  const home = fs.readFileSync('api/pages/home.html')
+  const file = path.join(process.cwd(), 'api/pages', 'home.html')
+  const home = fs.readFileSync(file)
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(home)
 })
