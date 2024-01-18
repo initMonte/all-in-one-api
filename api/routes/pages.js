e3a0e9f9de008bbe59cdb3523fdeb001609fad02
Matias Monteverde <initmonte@gmail.com>
2024-01-18 00:13:19 -0300

    Added content Type

--- api/routes/pages.js
+++ api/routes/pages.js
@@ -5,13 +5,16 @@ export const pagesRouter = Router()
 
 pagesRouter.get('/', (req, res) => {
   const home = fs.readFileSync('api/pages/home.html')
+  res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(home)
 })
 pagesRouter.get('/docs', (req, res) => {
   const docs = fs.readFileSync('api/pages/docs.html')
+  res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(docs)
 })
 pagesRouter.get('/*', (req, res) => {
   const notFound = fs.readFileSync('api/pages/notFound.html')
+  res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(notFound)
 })
