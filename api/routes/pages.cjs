d5aece6e82dab32857e6f17c363206f1bbef36d0
Matias Monteverde <initmonte@gmail.com>
2024-01-19 16:35:44 -0300

    trying routes to css and ico

--- api/routes/pages.cjs
+++ api/routes/pages.cjs
@@ -16,6 +16,18 @@ pagesRouter.get('/docs', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' })
   res.end(docs)
 })
+pagesRouter.get('/favicon.ico', (req, res) => {
+  const file = path.join(process.cwd(), '/', 'favicon.ico')
+  const ico = fs.readFileSync(file)
+  res.writeHead(200, { 'Content-Type': 'image/png' })
+  res.end(ico)
+})
+pagesRouter.get('/global.css', (req, res) => {
+  const file = path.join(process.cwd(), 'api/pages/css', 'global.css')
+  const css = fs.readFileSync(file)
+  res.writeHead(200, { 'Content-Type': 'text/css' })
+  res.end(css)
+})
 pagesRouter.get('/*', (req, res) => {
   const file = path.join(process.cwd(), 'api/pages', 'notFound.html')
   const notFound = fs.readFileSync(file)
